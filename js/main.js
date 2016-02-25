df1ac5bc63e86803573f9dca45d91b481277d6f0
Taru Karttunen <taruti@taruti.net>
2016-02-25 15:24:03 +0200

    Draw first bits

--- js/main.js
+++ js/main.js
@@ -4,5 +4,17 @@ function mainInit() {
     renderer = PIXI.autoDetectRenderer(800,600, {backgroundColor: 0x44FFFF});
     document.body.appendChild(renderer.view);
     stage = new PIXI.Container();
+    stage.addChild(bar2(50,50, 10, 100, 0x000000, 0xFFFF00));
     renderer.render(stage);
 }
+function bar2(x1, y1, w, h, c1, c2) {
+    var graphics = new PIXI.Graphics();
+    graphics.beginFill(c1);
+    graphics.drawRect(x1, y1, w, h/2);
+    graphics.endFill();
+    graphics.beginFill(c2);
+    graphics.drawRect(x1, y1+(h/2), w, h/2);
+    graphics.endFill();
+    return graphics
+}
+
