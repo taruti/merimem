bcf5a0e76f4a0b5769db0972270509a8f0c7e113
Taru Karttunen <taruti@taruti.net>
2016-02-26 13:03:41 +0200

    Add bar3

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function mainInit() {
     document.body.appendChild(renderer.view);
     stage = new PIXI.Container();
     stage.addChild(bar2(50,50, 10, 100, 0x000000, 0xFFFF00));
+    stage.addChild(bar3(80,50, 10, 100, 0x000000, 0xFFFF00, 0x000000));
     renderer.render(stage);
 }
 function bar2(x1, y1, w, h, c1, c2) {
@@ -14,6 +15,19 @@ function bar2(x1, y1, w, h, c1, c2) {
     graphics.endFill();
     graphics.beginFill(c2);
     graphics.drawRect(x1, y1+(h/2), w, h/2);
+    graphics.endFill();
+    return graphics
+}
+function bar3(x1, y1, w, h, c1, c2, c3) {
+    var graphics = new PIXI.Graphics();
+    graphics.beginFill(c1);
+    graphics.drawRect(x1, y1, w, h/3);
+    graphics.endFill();
+    graphics.beginFill(c2);
+    graphics.drawRect(x1, y1+(h/3), w, h/3);
+    graphics.endFill();
+    graphics.beginFill(c3);
+    graphics.drawRect(x1, y1+(2*h/3), w, h/3);
     graphics.endFill();
     return graphics
 }
