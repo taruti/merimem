a25d158279bc95fe71f7acb3131ec2ead62ba41b
Taru Karttunen <taruti@taruti.net>
2016-02-29 11:56:18 +0200

    Triangles

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ function mainInit() {
     stage = new PIXI.Container();
     stage.addChild(bar(50,50, 10, 100, 0x000000, 0xFFFF00));
     stage.addChild(bar(80,50, 10, 100, 0x000000, 0xFFFF00, 0x000000));
+    stage.addChild(triangle(120, 50, 20, 20, 0));
     renderer.render(stage);
 }
 
@@ -17,5 +18,13 @@ function bar(x1, y1, w, h) {
 	graphics.drawRect(x1, y1+((i-4)*nh), w, nh);
 	graphics.endFill();
     }
+    return graphics;
+}
+
+function triangle(x1, y1, w, h, c) {
+    var graphics = new PIXI.Graphics();
+    graphics.beginFill(c);
+    graphics.drawPolygon(x1, y1, x1+(w/2), y1+h, x1+w, y1);
+    graphics.endFill();
     return graphics;
 }
